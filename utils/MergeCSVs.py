1551cb57ab21364a4e96fa109786ccb0a4ccc3a0
Berteh <berteh@gmail.com>
2019-04-14 00:47:42 +0200

    Create MergeCSVs.py
    as proposed by @S1SYPHOS in https://github.com/berteh/ScribusGenerator/issues/132

---
+++ utils/MergeCSVs.py
@@ -0,0 +1,10 @@
+# merge all columns of the csv file in current directory into a single 'merge.csv' file. 
+# requires pandas librairy to be installed.
+# you can customize the merge in many ways: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.concat.html
+
+import pandas as pd
+import glob
+
+dfs = glob.glob('*.csv')
+result = pd.concat([pd.read_csv(df, sep=';') for df in dfs], ignore_index=True)
+result.to_csv('merge.csv')
