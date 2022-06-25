80f1bb4f2bd8e3a09ece18b81dbb1f554fa8c504
Hynek Cernoch <hynek@sdb.cz>
2022-06-25 19:09:36 +0200

    Fix ScribusGeneratorCLI help #200

--- ScribusGeneratorCLI.py
+++ ScribusGeneratorCLI.py
@@ -80,7 +80,7 @@ parser.add_argument('-e', '--csvEncoding', default=CONST.CSV_ENCODING,
 # parser.add_argument('-f', '--fast', '--noPdf', action='store_true', default=False, # commented utile Scribus allows pdf generation from command line
 #    help='no PDF generation, scribus SLA only (much faster)')
 parser.add_argument('-n', '--outName', default=CONST.EMPTY,
-                    help='name of the generated files, with no extension. Default is a simple incremental index. Using SG variables is allowed to define the name of generated documents. Use %VAR_COUNT% as a unique counter defined automatically from the data entry position.')
+                    help='name of the generated files, with no extension. Default is a simple incremental index. Using SG variables is allowed to define the name of generated documents. Use %%VAR_COUNT%% as a unique counter defined automatically from the data entry position.')
 parser.add_argument('-o', '--outDir', default=None,
                     help='directory were generated files are stored. Default is the directory of the scribus source file. outputDir will be created if it does not exist.')
 # parser.add_argument('-p', '--pdfOnly', '--noSla', action='store_true', default=False, # for pdf from CLI
